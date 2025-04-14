8cb7964399355397b2ef9ba6b74e731225e27f4a
Hasan Yurdakul <65603504+hasanyurdakul@users.noreply.github.com>
2025-04-14 22:35:45 +0300

    fix: metro.config.js entry point error

--- metro.config.js
+++ metro.config.js
@@ -4,28 +4,33 @@ const { withNativeWind } = require('nativewind/metro');
 const path = require('path');
 
 /** @type {import('expo/metro-config').MetroConfig} */
-// eslint-disable-next-line no-undef
-const config = {
-    watchFolders: [
-      path.resolve(__dirname, './ffmpeg-kit/react-native'),
-    ],
-    resolver: {
-      extraNodeModules: {
-        'react-native': path.resolve(__dirname, 'node_modules/react-native'),
-        'react': path.resolve(__dirname, 'node_modules/react'),
-        '@babel/runtime': path.resolve(__dirname, 'node_modules/@babel/runtime'),
-      },
-      blockList: [
-        /.*\/ffmpeg-kit\/react-native\/node_modules\/.*/,
-      ],
+const config = getDefaultConfig(__dirname);
+
+// Add ffmpeg-kit configuration
+config.watchFolders = [
+  ...(config.watchFolders || []),
+  path.resolve(__dirname, './ffmpeg-kit/react-native'),
+];
+
+config.resolver = {
+  ...config.resolver,
+  extraNodeModules: {
+    'react-native': path.resolve(__dirname, 'node_modules/react-native'),
+    'react': path.resolve(__dirname, 'node_modules/react'),
+    '@babel/runtime': path.resolve(__dirname, 'node_modules/@babel/runtime'),
+  },
+  blockList: [
+    /.*\/ffmpeg-kit\/react-native\/node_modules\/.*/,
+  ],
+};
+
+config.transformer = {
+  ...config.transformer,
+  getTransformOptions: async () => ({
+    transform: {
+      inlineRequires: true,
     },
-    transformer: {
-      getTransformOptions: async () => ({
-        transform: {
-          inlineRequires: true,
-        },
-      }),
-    },
-  };
+  }),
+};
 
 module.exports = withNativeWind(config, { input: './global.css' });
