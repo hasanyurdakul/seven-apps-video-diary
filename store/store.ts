3ad4a7905174ea99c798d9af12bc171dc7fec376
Hasan Yurdakul <65603504+hasanyurdakul@users.noreply.github.com>
2025-04-15 21:46:37 +0300

    fix:deprecated

--- store/store.ts
+++ store/store.ts
@@ -20,16 +20,27 @@ export const useVideoDiaryStore = create<VideoDiaryState>()(
   persist(
     (set, get) => ({
       videos: [],
-      addVideo: (video) => set({ videos: [...get().videos, video] }),
+      addVideo: video => set({ videos: [...get().videos, video] }),
       updateVideo: (id, updatedVideo) =>
         set({
-          videos: get().videos.map((v) => (v.id === id ? { ...v, ...updatedVideo } : v)),
+          videos: get().videos.map(v => (v.id === id ? { ...v, ...updatedVideo } : v)),
         }),
-      deleteVideo: (id) => set({ videos: get().videos.filter((v) => v.id !== id) }),
+      deleteVideo: id => set({ videos: get().videos.filter(v => v.id !== id) }),
     }),
     {
-      name: 'video-diary-storage', // unique name
-      getStorage: () => AsyncStorage, // (optional) by default the 'localStorage' is used
+      name: 'video-diary-storage',
+      storage: {
+        getItem: async name => {
+          const value = await AsyncStorage.getItem(name);
+          return value ? JSON.parse(value) : null;
+        },
+        setItem: async (name, value) => {
+          await AsyncStorage.setItem(name, JSON.stringify(value));
+        },
+        removeItem: async name => {
+          await AsyncStorage.removeItem(name);
+        },
+      },
     }
   )
 );
